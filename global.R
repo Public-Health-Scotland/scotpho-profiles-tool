7b1728bcb664311c621192b32ef911d0ddf8196b
Jaime Villacampa <jaime.villacampa@nhs.net>
2018-01-31 14:18:29 +0000

    Defining HSC partneship and locality. brining geographic lookup. Cleaning packages

--- global.R
+++ global.R
@@ -14,46 +14,38 @@ library (DT) # for data tables
 library(stringr) # dealing with string variables
 library(mgcv) #modelling
 library(leaflet) #javascript maps
-library(dygraphs) #time trend graphs
 library(reshape2) #for dygraph data modification
 library (rgdal) #for reading shapefiles
-library(ggiraph) #more interactive graphs -testing
-library(scales) #for scaling variables for spine chart
 library(plotly) #interactive graphs
-#library(rmapshaper) #for reducing size of shapefiles -not needed for app,just for modifying shp
 library(data.table) #for quick reading of csv files - Does not work with the actual version
 #library(gridExtra) #for combining graph and table (spine chart)
-
-#library(shinydashboard) #testing it: layout for shiny
-#library(metricsgraphics) #interactive graphics -testing
-#library(highcharter) #more interactive graphs -testing, problem with license
-#library(rbokeh) #more interactive graphs -testing
-#library(taucharts) #more interactive graphs -testing Cannot use due to R version
-
 
 ###############################################.
 ## Data ----
 ###############################################.    
 optdata <- readRDS("./data/optdata_test.rds") 
 
+geo_lookup <- readRDS("./data/geo_lookup.rds") #geography lookup
+
 #Geographies names
-hb_name <- unique(optdata$areaname[optdata$areatype=="Health board"]) %>% droplevels()
-la_name <- unique(optdata$areaname[optdata$areatype=="Council area"]) %>% droplevels()
-intzone_name <- unique(optdata$areaname[optdata$areatype=="Intermediate zone"]) %>% droplevels()
-partnership_name <- unique(optdata$areaname[optdata$areatype=="Partnership"]) %>% droplevels()
-locality_name <- unique(optdata$areaname[optdata$areatype=="Locality"]) %>% droplevels()
+hb_name <- unique(geo_lookup$areaname[geo_lookup$areatype=="Health board"]) %>% droplevels()
+la_name <- unique(geo_lookup$areaname[geo_lookup$areatype=="Council area"]) %>% droplevels()
+intzone_name <- unique(geo_lookup$areaname[geo_lookup$areatype=="Intermediate zone"]) %>% droplevels()
+partnership_name <- unique(geo_lookup$areaname[geo_lookup$areatype=="HSC Partnership"]) %>% droplevels()
+locality_name <- unique(geo_lookup$areaname[geo_lookup$areatype=="HSC Locality"]) %>% droplevels()
 
 #Lists of names used in dropdowns
 indicator_list <- unique(optdata$indicator)
-area_list <- unique(optdata$areaname)
-topic_list <- c(unique(optdata$topic1), unique(optdata$topic2))
-areatype_list <- c("Scotland", "Health board", "Council area", "Partnership", 
-                   "Locality", "Intermediate zone")
-areatype_noscot_list <- c("Health board", "Council area", "Partnership", 
-                         "Locality", "Intermediate zone")
+area_list <- unique(geo_lookup$areaname)
+topic_list <- c(unique(as.character(optdata$topic1)), unique(as.character(optdata$topic2)))
+areatype_list <- c("Scotland", "Health board", "Council area", "HSC Partnership", 
+                   "HSC Locality", "Intermediate zone")
+areatype_noscot_list <- c("Health board", "Council area", "HSC Partnership", 
+                         "HSC Locality", "Intermediate zone")
 
 #Palette for time trend.
-trend_pal <-  c('#08519c','#bdd7e7','#3182bd', '#6baed6', '#eff3ff')
+trend_pal <-  c('#2166ac','#4393c3', '#92c5de', '#d1e5f0', '#053061',
+                '#8c510a', '#bf812d', '#dfc27d', '#f6e8c3', '#543005')
 
 ##########.
 #Map data
@@ -63,8 +55,9 @@ HB_bound<-readOGR("./shapefiles","HB_simpl") #Reading file with health board sha
 
 # Identify which geographies have data for each indicator
 indic <- unique(optdata$indicator[!is.na(optdata$measure)])
-indic_geog <- tapply(optdata$areaname[!is.na(optdata$measure)], optdata$indicator[!is.na(optdata$measure)], unique)
-indic_geog_type <- tapply(optdata$areatype[!is.na(optdata$measure)], optdata$indicator[!is.na(optdata$measure)], unique)
+indic_geog <- tapply(optdata$code[!is.na(optdata$measure)], optdata$indicator[!is.na(optdata$measure)], unique)
+#It seems I was not using it
+#indic_geog_type <- tapply(optdata$areatype[!is.na(optdata$measure)], optdata$indicator[!is.na(optdata$measure)], unique)
 
 
 ##END
