6b3d1b6635209d5ad1a285ab0343e74cfefff48f
Jaime Villacampa <jaime.villacampa@nhs.net>
2018-03-21 12:28:43 +0000

    Rsession info 21 March 2018

---
+++ rsessioninfo.R
@@ -0,0 +1,33 @@
+#Rsession information
+
+# R version 3.3.2 (2016-10-31)
+# Platform: i386-w64-mingw32/i386 (32-bit)
+# Running under: Windows 7 (build 7601) Service Pack 1
+# 
+# locale:
+#   [1] LC_COLLATE=English_United Kingdom.1252  LC_CTYPE=English_United Kingdom.1252   
+# [3] LC_MONETARY=English_United Kingdom.1252 LC_NUMERIC=C                           
+# [5] LC_TIME=English_United Kingdom.1252    
+# 
+# attached base packages:
+#   [1] stats     graphics  grDevices utils     datasets  methods   base     
+# 
+# other attached packages:
+#   [1] sp_1.2-5          plotly_4.7.1      leaflet_1.1.0     DT_0.2            dplyr_0.7.2      
+# [6] purrr_0.2.3       readr_1.1.1       tidyr_0.7.0       tibble_1.3.4      ggplot2_2.2.1    
+# [11] tidyverse_1.1.1   shinythemes_1.1.1 shiny_1.0.5      
+# 
+# loaded via a namespace (and not attached):
+#   [1] jsonlite_1.5        reshape2_1.4.2      magrittr_1.5        gtable_0.2.0       
+# [5] data.table_1.10.4-2 hms_0.3             xml2_1.1.1          htmltools_0.3.6    
+# [9] forcats_0.2.0       stringr_1.2.0       haven_1.1.0         broom_0.4.2        
+# [13] cellranger_1.1.0    lattice_0.20-34     htmlwidgets_0.9     plyr_1.8.4         
+# [17] lubridate_1.6.0     mime_0.5            pkgconfig_2.0.1     R6_2.2.2           
+# [21] digest_0.6.12       colorspace_1.3-2    bindrcpp_0.2        stringi_1.1.5      
+# [25] lazyeval_0.2.0      yaml_2.1.14         crosstalk_1.0.0     httr_1.3.1         
+# [29] bindr_0.1           xtable_1.8-2        munsell_0.4.3       psych_1.7.5        
+# [33] modelr_0.1.1        Rcpp_0.12.12        readxl_1.0.0        parallel_3.3.2     
+# [37] assertthat_0.2.0    tools_3.3.2         foreign_0.8-67      httpuv_1.3.5       
+# [41] viridisLite_0.2.0   scales_0.5.0        glue_1.1.1          rlang_0.1.2        
+# [45] mnormt_1.5-5        nlme_3.1-128        rvest_0.3.2         rsconnect_0.8.5    
+# [49] grid_3.3.2       
