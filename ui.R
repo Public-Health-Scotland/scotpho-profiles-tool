d869a8fe90046d31a6ba6a34cac1cbc11c0cd3ae
Jaime Villacampa <jaime.villacampa@nhs.net>
2018-04-05 14:27:39 +0100

    Taking out fluidPage. Incorporating logo in navigation bar. Style of text, selectors and buttons. Reorganizing overview and rank filters. New legend style. Taking out text when not adding anything.

--- ui.R
+++ ui.R
@@ -7,37 +7,47 @@
 ###############################################.
 ## Header ---- 
 ###############################################.
-fluidPage(theme = shinytheme("cerulean"), #Theme of the app (blue navbar)
-          tags$head( #CSS styles
-            #color sidebars/well panels
-            tags$style(".well {background-color:#ffffff; border: 0px solid #336699;}"), 
-            #Style for download buttons
-            tags$style(".down{background-color:#4da6ff;} .down{color: white;} 
-                       .down{background-image:none;}"), 
-            #to avoid red text error messages in the whole app, take out for testing
-            tags$style(type="text/css", ".shiny-output-error { visibility: hidden; }",
-              ".shiny-output-error:before { visibility: hidden; }" ),
-            #External links underlined an open a new tab
-            tags$style(".externallink{text-decoration: underline;} "),
-            HTML("<base target='_blank'>")
-          ),
-#Header style and content, using html tags
-  div(style="height: 80px;  ", 
-    tags$a( #image that links to ScotPHO main page
-      img(src='scotpho_logo.png', height=70, style="float: left; padding-right: 40px;"),
-      href= "http://www.scotpho.org" 
-    ),
-    h3("ScotPHO Profiles", 
-       style="vertical-align: bottom; padding-top:25px; "), 
-    #asking for feedback TAKE OUT after beta period
-    h6(tags$a(href="mailto:ScotPHO@nhs.net", "Tell us what you think of our new tool!"))
-  ),
-  navbarPage("", # Navigation bar
+# fluidPage(
+# #Header style and content, using html tags
+#   div(style="height: 80px;  ", 
+#     tags$a( #image that links to ScotPHO main page
+#       img(src='scotpho_logo.png', height=70, style="float: left; padding-right: 40px;"),
+#       href= "http://www.scotpho.org" 
+#     ),
+#     h3("ScotPHO Profiles", 
+#        style="vertical-align: bottom; padding-top:25px; "), 
+#     #asking for feedback TAKE OUT after beta period
+#     h6(tags$a(href="mailto:ScotPHO@nhs.net", "Tell us what you think of our new tool!"))
+#   ),
+  navbarPage(title = div(img(src="scotpho_reduced.png", height=40), 
+                         style = "position: relative; top: -5px;"), # Navigation bar
+             theme = shinytheme("cerulean"), #Theme of the app (blue navbar)
+             collapsible = TRUE, #tab panels collapse into menu in small screens
+             # shinythemes::themeSelector(),
+             header =         
+             tags$head( #CSS styles
+               beta_box,  ##### Feedback box. TO TAKE OUT AFTER BETA PERIOD
+               #Style sidebars/well panels
+               tags$style(".well {background-color:#ffffff; border: 0px solid #336699;}",
+               #Style dropdown menus and labels
+               ".form-group {margin: 0px}",
+               #Text for the whole app and for navbar
+               "body { font-size: 12px}",
+               ".navbar { font-size: 14px}",
+               #Style for download buttons
+               ".down{background-color:#4da6ff; color: white; background-image:none;
+               font-size: 12px; padding: 5px 10px}",
+               #to avoid red text error messages in the whole app, take out for testing
+               ".shiny-output-error { visibility: hidden; }",
+                          ".shiny-output-error:before { visibility: hidden; }",
+               #External links underlined an open a new tab
+               ".externallink{text-decoration: underline;} "),
+               HTML("<base target='_blank'>")
+               ),
 ###############################################.
 ## Overview ----
 ###############################################.
 tabPanel("Overview", icon = icon("heartbeat"),
-         beta_box,  
          p(tags$b("Explore how an area compares to Scotland (or a comparator of your choice) 
            over time.")), #Intro text
          tags$ul( 
@@ -46,24 +56,33 @@ tabPanel("Overview", icon = icon("heartbeat"),
                    the most recent data on the right."),
            tags$li("Hover over each tile to see indicator definitions and time periods.")),
            wellPanel( #Filter options
-             column(8,
-                    selectInput("geotype_heat", "Geography level", 
-                                choices= areatype_noscot_list),
+             column(4,
+                    selectInput("geotype_heat", "Geography level", choices= areatype_noscot_list),
                     conditionalPanel(#Conditional panel for extra dropdown for localities & IZ
                       condition = "input.geotype_heat== 'HSC Locality' | input.geotype_heat == 'Intermediate zone' ",
                       selectInput("loc_iz_heat", label = "Partnership for localities/intermediate zones",
                                   choices = partnership_name)
                     ),
-                    uiOutput("geoname_ui_heat"),
-                    selectInput("geocomp_heat", "Comparator", choices = comparator_list,
-                                selectize=TRUE, selected = "Scotland")
+                    uiOutput("geoname_ui_heat")
              ),
              column(4,
                     selectInput("topic_heat", "Topic", choices = topic_list,
                                 selectize=TRUE, selected = "Scotland"),
-                    downloadButton('download_heat', 'Download data', class = "down"),
-                    h5(tags$b("Legend"), style="color: black;"),
-                    img(src='legend_rank.png', height = 130)
+                    selectInput("geocomp_heat", "Comparator", choices = comparator_list,
+                                selectize=TRUE, selected = "Scotland")
+             ),
+             column(4,
+                    #Legend
+                    h6(tags$b("Legend"), style="color: black;"),
+                    p(img(src='non_signif.png', height=12, style="padding-right: 2px; vertical-align:middle"), 
+                      "Statistically not significantly different from comparator average.", br(),
+                      img(src='signif_worse.png', height=12, style="padding-right: 2px; vertical-align:middle"), 
+                      "Statistically significantly worse than comparator average.", br(),
+                      img(src='signif_better.png', height=12, style="padding-right: 2px; vertical-align:middle"), 
+                      "Statistically significantly better than comparator average.", br(),
+                      img(src='signif_nocalc.png', height=12, style="padding-right: 2px; vertical-align:middle"), 
+                      "No significance can be calculated."),
+                    downloadButton('download_heat', 'Download data', class = "down")
              )
            ),
            mainPanel(width=12, #Main panel
@@ -74,14 +93,6 @@ tabPanel("Overview", icon = icon("heartbeat"),
 ## Time trend ----
 ###############################################.
 tabPanel("Trend", icon = icon("area-chart"),
-         beta_box,  
-         p(tags$b("Explore changes over time. ")), #Intro text
-         tags$ul( 
-           tags$li("Use menus to select an indicator of interest and the geographies 
-                   you wish to display."),
-           tags$li("Some indicator data is not available for all geographies."),
-           tags$li("The ‘Download data’ button will download and save the chart data 
-                   as a csv file.")),
          wellPanel( #Filter options
                    column(4,
                           selectInput("indic_trend", "Indicator", choices=indicator_list),
@@ -117,36 +128,37 @@ tabPanel("Trend", icon = icon("area-chart"),
 ## Rank chart ---- 
 ###############################################.
 tabPanel("Rank", icon = icon("signal"),
-         beta_box,  
-         p(tags$b(" Compare geographical areas. ")), #Intro text
-         tags$ul( 
-           tags$li("Use menus to select an indicator of interest, time period and 
-                   the geography level for comparison."),
-           tags$li("Some indicator data is not available for all geographies."),
-           tags$li("The ‘Download data’ button will download and save the chart data 
-                   as a csv file.")),
          wellPanel( #Filter options
-           column(6,
+           column(4,
+                  selectInput("indic_rank", "Indicator", choices=indicator_list),
                   selectInput("geotype_rank", label = "Geography level",
                               choices = areatype_noscot_list, selected = "Health board"),
                   conditionalPanel( #Conditional panel for extra dropdown for localities & IZ
                     condition = "input.geotype_rank == 'HSC Locality' | input.geotype_rank == 'Intermediate zone' ",
                     selectInput("loc_iz_rank", label = "Partnership for localities/intermediate zones",
                                 choices = partnership_name)
-                  ),
-                  checkboxInput("ci_rank",  
-                                label = "95% confidence intervals", value = FALSE),
-                  downloadButton('download_rank', 'Download data', class = "down"),
-                  h5(tags$b("Legend"), style="color: black;"),
-                  img(src='legend_rank.png', height = 130)
-           ),  
-           column(6,
-                  selectInput("indic_rank", "Indicator", choices=indicator_list),
+                  )
+                ),
+           column(3,
                   uiOutput("year_ui_rank"), 
                   selectInput("geocomp_rank", "Comparator", choices = comparator_list,
-                              selectize=TRUE, selected = "Scotland")
-           )  
-         ),
+                              selectize=TRUE, selected = "Scotland"),
+                  checkboxInput("ci_rank",label = "95% confidence intervals", value = FALSE)
+           ), 
+            column(5,
+                  #Legend
+                  h6(tags$b("Legend"), style="color: black;"),
+                  p(img(src='non_signif.png', height=12, style="padding-right: 2px; vertical-align:middle"), 
+                    "Statistically not significantly different from comparator average.", br(),
+                    img(src='signif_worse.png', height=12, style="padding-right: 2px; vertical-align:middle"), 
+                    "Statistically significantly worse than comparator average.", br(),
+                    img(src='signif_better.png', height=12, style="padding-right: 2px; vertical-align:middle"), 
+                    "Statistically significantly better than comparator average.", br(),
+                    img(src='signif_nocalc2.png', height=12, style="padding-right: 2px; vertical-align:middle"), 
+                    "No significance can be calculated."),
+                  downloadButton('download_rank', 'Download data', class = "down")
+                  )
+          ),
          mainPanel(width = 12, #Main panel
                    plotlyOutput("rank_plot") 
          )
@@ -155,18 +167,13 @@ tabPanel("Rank", icon = icon("signal"),
 ## Deprivation ---- 
 ###############################################.
 tabPanel("Deprivation", icon = icon("balance-scale"),
-         beta_box,  
          #Intro text
          p(tags$b("This section is under development and a limited number of indicators are available."), style = "color: red "),
          p(tags$b("Explore the data by different levels of deprivation. ")),
          tags$ul( 
            tags$li("ScotPHO are working on new ways to display information related to", 
                    tags$a(href="http://www.scotpho.org.uk/life-circumstances/deprivation/key-points/", "deprivation",
-                          class="externallink"), "."),
-           tags$li("Use menus to select an indicator of interest and the geographies 
-                   you wish to display."),
-           tags$li("The ‘Download data’ button will download and save the charts data 
-                   as a csv file.")),
+                          class="externallink"), ".")),
          wellPanel( #Filter options
                    column(6, selectInput("indic_simd", label = "Indicator",
                                          choices = ind_depr_list)),
@@ -176,7 +183,8 @@ tabPanel("Deprivation", icon = icon("balance-scale"),
                    column(5, uiOutput("year_ui_simd")),
                    column(5, selectInput("measure_simd", label = "Type of measure",
                                          choices = c("Rate/Percentage", "Index of inequality"))),
-                   column(2, downloadButton(outputId = 'download_simd', class = "down"))
+                   column(2, downloadButton(outputId = 'download_simd',
+                                            "Download data",class = "down"))
          ),
          mainPanel(width = 12, #Main panel
                    column(6,
@@ -189,24 +197,16 @@ tabPanel("Deprivation", icon = icon("balance-scale"),
 ###########Map ----
 ###############################################.
 tabPanel("Map", icon = icon("globe"),
-         beta_box,  
-         #Intro text
-         p(tags$b("Indicator information shown in a map. ")),
-         tags$ul( 
-           tags$li("Use menus to select an indicator of interest, a time period, 
-                    and the geographies you wish to display."),
-           tags$li("The ‘Download data’ button will download and save the map data 
-                   as a csv file.")),
          sidebarPanel(    
            selectInput("indic_map", "Indicator", choices=indicator_map_list),
            uiOutput("year_ui_map"),
            downloadButton('download_map', 'Download data', class = "down"),
            shiny::hr(),
-           h5(tags$b("Legend")),
+           h6(tags$b("Legend"), style="color: black;"),
            img(src='legend_map.png', height=150, style = "align: right")
          ), 
          mainPanel( #Main panel
-           h4(textOutput("title_map")),
+           h5(textOutput("title_map"), style="color: black;"),
            leafletOutput("map")
          )
     ), #Tab panel bracket
@@ -214,25 +214,21 @@ tabPanel("Map", icon = icon("globe"),
 ## Table ----
 ###############################################.
 tabPanel("Table", icon = icon("table"),
-         beta_box,  
          #Intro text
          column(9,
                 p(tags$b("Indicator data in a table format. ")),
                 tags$ul( 
-                  tags$li("Use the filters to select the data you are interested in."),
-                  tags$li("The ‘Download data’ button will download and save the table data 
-                          as a csv file."))
+                  tags$li("Use the filters to select the data you are interested in."))
                 ),
          column(3,
                 downloadButton('download_table', 'Download data', class = "down")  #For downloading the data
          ),
-         div(DT::dataTableOutput("table_opt"), style = "font-size:70%") #table
+         div(DT::dataTableOutput("table_opt"), style = "font-size:90%") #table
   ), #Tab panel bracket
 ###############################################.             
 ##############Help----    
 ###############################################.
 tabPanel("Help", icon = icon("question"),
-         beta_box,       
          p(tags$b("Welcome to the ScotPHO Profiles Tool "), "designed to allow users 
            to view the various different profiles produced by the ScotPHO collaboration."),
          p("The profiles are intended to increase understanding of local health issues 
@@ -276,6 +272,6 @@ tabPanel("Help", icon = icon("question"),
              )
          ) #Tab panel bracket
   )#, #Bracket  navbarPage
-) # Bracket fluidPage
+#) # Bracket fluidPage
 
 ##END
