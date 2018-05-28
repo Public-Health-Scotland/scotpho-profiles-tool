9ed26951035ce3a578b16895227bdf8fa2ec41fc
vicky e <victoria.elliott@nhs.net>
2018-05-28 13:49:00 +0100

    test

--- deploying app.R
+++ deploying app.R
@@ -1,8 +1,8 @@
 #Syntax to deploy the data explorer app to the shiny.io server
 
-##################
+##################.
 #Connection to server ----
-##################
+##################.
 library(rsconnect)
 #Proxy options, needed to carry out the connection.
 #It will prompt a screen where you have to include your NSS login details
@@ -14,23 +14,23 @@ options(RCurlOptions = list(proxy = "PROXY.NSS.SCOT.NHS.UK:3128",
 #Connecting to the shyny.io account.
 #Enter the token and secret provided by one of the account managers
 rsconnect::setAccountInfo(name='scotland',
-                          token='5240F040CC78944A7C7CA52907838CAF',
-                          secret='NlayZ8YrtbBfz3hRI3lNQd/baWQ5z+P/aOvSPFjr')
+                          token='2CA75B26108872D3EA1FDB0665CF1B80',
+                          secret='o8mkNkleSR0N4lPezhEjJrs7eZmcCXRzAc8kNl2b')
 
-##################
+##################.
 #Live app ----
-##################
+##################.
 rsconnect::deployApp('//stats/phip/Projects/Profiles/R Shiny/ScotPHO_profiles',
                      appName="ScotPHO_profiles_tool")
 
-##################
+##################.
 #Test app ----
-##################
+##################.
 rsconnect::deployApp('//stats/phip/Projects/Profiles/R Shiny/ScotPHO_profiles',
                      appName="ScotPHO_profiles_tool_test")
 
 
-###########################################################
+###########################################################.
 ##END
 
 
