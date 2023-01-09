07ed94aa89341aa352b4a125542e12fc4f7f4029
Paul Fejiro <paulfe02@nssrstudio05.csa.scot.nhs.uk>
2023-01-09 09:53:19 +0000

    adding analytics code to track geographies

--- shiny_app/gtag.js
+++ shiny_app/gtag.js
@@ -3,6 +3,8 @@
   gtag('js', new Date());
 
   gtag('config', 'G-KE1C59RLNS'); 
+
+// Tracks tabs being selected 
    
   $(document).on('click', 'a', function(e) {
   gtag('event', 'tab_viewed', {'event_category' : 'TabsetPanel',
@@ -120,3 +122,175 @@
   });
   
 }); 
+
+// Track geographies being selected .........................................
+
+  $(document).on('shiny:inputchanged','#geotype_summary', function(f) {
+  gtag('event', 'geography_selected', {'event_category' : "summary_tab",
+    'event_label' : f.name,
+    'geography_name' : f.value
+  });
+  
+}); 
+
+  $(document).on('shiny:inputchanged','#loc_iz_summary', function(f) {
+  gtag('event', 'geography_selected', {'event_category' : "summary_tab",
+    'event_label' : f.name,
+    'geography_name' : f.value
+  });
+  
+}); 
+
+  $(document).on('shiny:inputchanged','#hbname_trend', function(f) {
+  gtag('event', 'geography_selected', {'event_category' : "trend_tab",
+    'event_label' : f.name,
+    'geography_name' : f.value
+  });
+  
+}); 
+
+  $(document).on('shiny:inputchanged','#partname_trend', function(f) {
+  gtag('event', 'geography_selected', {'event_category' : "trend_tab",
+    'event_label' : f.name,
+    'geography_name' : f.value
+  });
+  
+});
+
+  $(document).on('shiny:inputchanged','#caname_trend', function(f) {
+  gtag('event', 'geography_selected', {'event_category' : "trend_tab",
+    'event_label' : f.name,
+    'geography_name' : f.value
+  });
+  
+});
+
+  $(document).on('shiny:inputchanged','#adpname_trend', function(f) {
+  gtag('event', 'geography_selected', {'event_category' : "trend_tab",
+    'event_label' : f.name,
+    'geography_name' : f.value
+  });
+  
+});
+
+  $(document).on('shiny:inputchanged','#loc_iz_trend', function(f) {
+  gtag('event', 'geography_selected', {'event_category' : "trend_tab",
+    'event_label' : f.name,
+    'geography_name' : f.value
+  });
+  
+});
+
+  $(document).on('shiny:inputchanged','#loc_iz_rank', function(f) {
+  gtag('event', 'geography_selected', {'event_category' : "rank_tab",
+    'event_label' : f.name,
+    'geography_name' : f.value
+  });
+  
+});
+
+  $(document).on('shiny:inputchanged','#geocomp_rank', function(f) {
+  gtag('event', 'geography_selected', {'event_category' : "rank_tab",
+    'event_label' : f.name,
+    'geography_name' : f.value
+  });
+  
+});
+
+
+  $(document).on('shiny:inputchanged','#geotype_simd', function(f) {
+  gtag('event', 'geography_selected', {'event_category' : "simd_tab",
+    'event_label' : f.name,
+    'geography_name' : f.value
+  });
+  
+});
+
+  $(document).on('shiny:inputchanged','#hb_true', function(f) {
+  gtag('event', 'geography_selected', {'event_category' : "data_tab",
+    'event_label' : f.name,
+    'geography_name' : f.value
+  });
+  
+});
+
+  $(document).on('shiny:inputchanged','#la_true', function(f) {
+  gtag('event', 'geography_selected', {'event_category' : "data_tab",
+    'event_label' : f.name,
+    'geography_name' : f.value
+  });
+  
+});
+
+  $(document).on('shiny:inputchanged','#adp_true', function(f) {
+  gtag('event', 'geography_selected', {'event_category' : "data_tab",
+    'event_label' : f.name,
+    'geography_name' : f.value
+  });
+  
+});
+
+  $(document).on('shiny:inputchanged','#hscp_true', function(f) {
+  gtag('event', 'geography_selected', {'event_category' : "data_tab",
+    'event_label' : f.name,
+    'geography_name' : f.value
+  });
+  
+});
+
+  $(document).on('shiny:inputchanged','#hscl_parent', function(f) {
+  gtag('event', 'geography_selected', {'event_category' : "data_tab",
+    'event_label' : f.name,
+    'geography_name' : f.value
+  });
+  
+});
+
+  $(document).on('shiny:inputchanged','#iz_parent', function(f) {
+  gtag('event', 'geography_selected', {'event_category' : "data_tab",
+    'event_label' : f.name,
+    'geography_name' : f.value
+  });
+  
+});
+
+  $(document).on('shiny:inputchanged','#code', function(f) {
+  gtag('event', 'geography_selected', {'event_category' : "data_tab",
+    'event_label' : f.name,
+    'geography_name' : f.value
+  });
+  
+});
+
+  $(document).on('click', '#comp_rank1', function(e) {
+  gtag('event', 'comparison_selector', {'event_category' : 'rank_tab',
+    'event_label' : "area",
+    'tab_name' : "area"
+  });
+  
+});
+
+  $(document).on('click', '#comp_rank2', function(e) {
+  gtag('event', 'comparison_selector', {'event_category' : 'rank_tab',
+    'event_label' : "time",
+    'tab_name' : "time"
+  });
+  
+});
+
+  $(document).on('click', '#comp_summary1', function(e) {
+  gtag('event', 'comparison_selector', {'event_category' : 'summary_tab',
+    'event_label' : "area",
+    'tab_name' : "area"
+  });
+  
+});
+
+  $(document).on('click', '#comp_summary2', function(e) {
+  gtag('event', 'comparison_selector', {'event_category' : 'summary_tab',
+    'event_label' : "time",
+    'tab_name' : "time"
+  });
+  
+});
+
