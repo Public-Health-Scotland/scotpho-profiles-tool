1b85c26e0cbb63eb1fa503a66686eb6eb6ee8d4b
Monica McGibbon <monica.mcgibbon@phs.scot>
2023-06-26 09:39:14 +0100

    new summary ui

--- shiny_app/ui scripts/summaryUI.R
+++ shiny_app/ui scripts/summaryUI.R
@@ -1,95 +1,113 @@
-###############################################
-#
-# UI for summary tab
-#
-###############################################
+###############################################.
+## Summary ----
+###############################################.
+
+summaryTab <- tabPanel(
+  
+  div(div(class="fa fa-list-ul", role = "navigation"), "Summary"),
+  
 
 
+  div(style = "margin:10px;",
+      h3("Build a local area profile summary", style = "font-weight: bold;"),
+      
+      # sub-container for filters/guidance
+      div(class = "summary-instructions", style = "display:flex; padding:5px;",
+          
+          # filters
+          div(style = "display:flex; flex-direction:column; border-top:3px solid #d2d6de;",
+              
+              
+              div(style = "display:flex; align-items: center;",
+                  tags$i(class = "fa fa-filter", style = "color:#3F3685; font-size:2rem;"),
+                  h4("Filters", style = "margin-left:5px;")),
+              
+              # filter 1: scotpho profile 
+              selectInput("summary_profile", 
+                          label= "Step 1: Select a profile:", 
+                          choices = profile_list, 
+                          selected = "HWB"),
+              
+              # filter 2 : geography level
+              selectInput("geotype_summary", 
+                          label = "Step 2. Select a geography level and then an area of interest.", 
+                          choices=areatype_list,
+                          selected = "Health board"),
+              
+              # conditional filter if IZ/locality selected
+                         conditionalPanel(
+                           condition = "input.geotype_summary== 'HSC locality' | input.geotype_summary == 'Intermediate zone' ",
+                                            
+              selectInput("loc_iz_summary", 
+                          label = "Step 2b. Select a region for localities or intermediate zones",
+                          choices = partnership_name)),
+              
+              # filter 3: geography area 
+              # note: choices defined on server-side depending on chosen geography level
+              selectInput("geoname_summary", 
+                          label = NULL, 
+                          choices = NULL),
+              
+              
+              # downloads buttons
+              div(style = "display:flex; align-items: center;",
+                  tags$i(class = "fa fa-download", style = "color:#3F3685; font-size:2rem;"),
+                  h4("Downloads", style = "margin-left:5px;")),
+              
+              
+              # download as csv
+              downloadButton("download_summary_csv",
+                             "Download summary as CSV",
+                             class = "button")#,
+              
+              # download as pdf (in progress)
+              # actionButton("report", 
+              #              "Download summary as PDF",
+              #              class = "button")
+         
+          ), 
+          
+          
+          # table instructions
+          div(style = "padding:20px; margin-left: 20px; background-color:#F2F2F2; border-top:3px solid #d2d6de;",
+              div(style = "display:flex; align-items: center;",
+                  tags$i(class = "fa fa-info-circle", style = "color:#3F3685; font-size:2rem;"),
+                  h4("How to interpret table results", style = "margin-left:5px")),
+              
+    
+              p(style = "font-size:16px; padding:5px",
+              "The results below provide a snapshot of the latest data for a chosen profile and geographical area, compared to Scotland.
+               The colour of the result for your chosen area signals whether the results are statistically significantly different to Scotland."),
+              
+              fluidRow(
+                column(1, tags$span(class="dot", style = "height: 25px; width: 25px; background-color: orange; border-radius: 50%; display: inline-block;")),
+                column(9, p("Orange - statistically significantly worse than Scotland"))),
+              
+              fluidRow(
+                column(1, tags$span(class="dot", style = "height: 25px; width: 25px; background-color: #1B7CED; border-radius: 50%; display: inline-block;")),
+                column(9, p("Blue - statistically significantly better than Scotland"))),
+              
+              fluidRow(
+                column(1, tags$span(class="dot", style = "height: 25px; width: 25px; background-color: grey; border-radius: 50%; display: inline-block;")),
+                column(9, p("Grey - not statistically significantly different to Scotland"))),
+               p("The shaded blue bar shows the range of values for that indicator across all areas within your chosen geography level. 
+               The coloured dots show the relative position of your selected geography and the red line shows the value for Scotland. 
+                 and how far it is from the best and worse performing areas at your chosen geographical level.")
+          )
+          
+          
+      )
+  ),
+  
 
-summaryTab <- tabPanel("Summary", icon = icon("list-ul"), value = "summary",
-         introBox(
-           wellPanel(fluidRow( #Filter options
-             column(3,
-                    div(title="Select a geography level first, then select the are you want from the list. You can click in the box, hit backspace and start to type if you want to start searching.",
-                        p(tags$b("Step 1. Select a geography level and then an area of interest.")),
-                        selectInput("geotype_summary", label = NULL, choices=areatype_list,
-                                    selected = "Health board"),
-                        conditionalPanel(#Conditional panel for extra dropdown for localities & IZ
-                          condition = "input.geotype_summary== 'HSC locality' | input.geotype_summary == 'Intermediate zone' ",
-                          div(title="This option restricts the HSC locality or IZ options below to only areas within a parent geography",                      
-                              selectInput("loc_iz_summary", label = "Step 1b. Select a region for localities or intermediate zones",
-                                          choices = partnership_name))
-                        ),
-                        uiOutput("geoname_ui_summary"))
-             ),
-             column(3,
-                    div(title="Select the profile you are interested in. Not all profiles are available for all geographies",
-                        p(tags$b("Step 2. Select a profile ")),
-                        div(id= "summary_div", uiOutput("profile_ui_summary")),
-                        # domain if spine selected
-                        conditionalPanel(condition = 'input.chart_summary == "Spine"',
-                                         uiOutput("topic_ui_spine")))
-             ),
-             column(3,
-                    div(title="Compare against another area (e.g. Scotland) or against a previous period to see the evolution of the area",
-                        p(tags$b("Step 3. Select to compare by ")),
-                        awesomeRadio("comp_summary", label = NULL,
-                                     choices = list("Area or" = 1, "Time" = 2), 
-                                     selected = 1, inline=TRUE, checkbox = TRUE),
-                        uiOutput("comp_ui_summary")) # comparator options
-             ),
-             column(3,
-                    actionButton("help_summary",label="Help", icon= icon('question-circle'), class ="down"),
-                    actionButton("defs_summary",label="Definitions", icon= icon('info'), class ="down"),
-                    downloadButton('download_summary', 'Download data', class = "down"),
-                    uiOutput("save_chart_ui"))),
-             fluidRow(column(12,
-                             column(3),#empty column to replicate offset and center content
-                             column(6,
-                                    p(tags$b("Step 4. Select what type of summary you want to see:"), 
-                                      " snapshot is a comparison with the latest data available, 
-                                      trend will show how things are changing over time, and 
-                                      spine compares indicators with the rest of areas of the same level."),
-                                    radioGroupButtons("chart_summary", status = "primary", justified = TRUE,
-                                                      choices = c("Snapshot", "Trend", "Spine"), label=NULL  )),
-                             column(3) #empty column to replicate offset and center content
-                             )) # column and row brackets
-           ), #well panel bracket
-           data.step = 4, 
-           data.intro =(p(h5("Throughout the tool use the dropdown menus to change which indicators or geographies are displayed in the charts."),
-                          br(),
-                          h5("While using dropdown menus mouse click within a dropdown menu and press backspace on your keyboard ('<-') then start typing a word to quickly find the options you are looking for"),
-                          img(src='introjs_how_to_select.png')))
+  # summary table to be displayed ------
+  div(class = "tableContainer", style = "margin-left:10px; margin-right:10px;",
+               div(style = "display: flex; justify-content: space-between; background-color: hsl(205, 100%, 36%);",
+                   div(style = "color: white; margin-left: 10px;", uiOutput("profile_title")), 
+                   div(style = "margin: 5px;", img(src = "scotpho_reduced.png", width = 120))),
+               uiOutput("summary_table")
+      )
            
-           ), #introbox bracket
-         mainPanel(width = 12,
-                   shiny::hr(),
-                   bsModal("mod_defs_summary", "Definitions", "defs_summary",
-                           htmlOutput('defs_text_summary')),
-                   fluidRow(column(4,
-                                   h4(textOutput("summary_title"), style="color: black; text-align: left"),
-                                   h5(textOutput("summary_subtitle"), style="color: black; text-align: left")
-                   ),
-                   column(3,
-                          br(),
-                          br(),
-                          p(img(src='signif_better.png', height=18, style="padding-right: 2px; vertical-align:middle"), 
-                            "Better than comparator", br(),
-                            img(src='non_signif.png', height=18, style="padding-right: 2px; vertical-align:middle"), 
-                            "Not different to comparator", br(),
-                            img(src='signif_worse.png', height=18, style="padding-right: 2px; vertical-align:middle"), 
-                            "Worse than comparator", br(),
-                            img(src='signif_nocalc.png', height=18, style="padding-right: 2px; vertical-align:middle"), 
-                            "No differences can be calculated")),
-                   conditionalPanel(condition = 'input.chart_summary == "Spine"', 
-                                    column(5,
-                                           br(),
-                                           br(),
-                                           uiOutput("ui_spine_legend_selected"),
-                                           uiOutput("ui_spine_legend_areatype"),
-                                           uiOutput("ui_spine_legend_comparator")))),
-                   # Depending what users selects different visualizations
-                   uiOutput("summary_expl_text"),
-                   uiOutput("summary_ui_plots")
-         )
-           ) #Tab panel bracket
+)
+
+
