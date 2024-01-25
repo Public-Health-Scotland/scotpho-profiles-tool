9903442a2b33c79656a93f51d8a744d92017466a
Monica McGibbon <monica.mcgibbon@phs.scot>
2024-01-25 14:25:56 +0000

    new data tab ui

--- shiny_app/ui scripts/dataUI.R
+++ shiny_app/ui scripts/dataUI.R
@@ -5,122 +5,92 @@
 ###############################################
 
 
-dataTab <- 
-
-tabPanel(div(
+dataTab <-
+ tabPanel(
+  title = div(
   div(class="fa fa-table", role = "navigation"),
   "Data"),
   value = "table",
-  #Sidepanel for filtering data
-  mainPanel(
-    width = 12, style="margin-left:0.5%; margin-right:0.5%",
-    #Row 1 for intro  
-    fluidRow(
-      p("Download the data used in the tool", 
-        style = "font-weight: bold; color: black;"),
-      p("Use the filters below to select the data you want to download. ",
-        "To delete choices use backspace or select item and delete"),
-      br()
-    ),
-    #Row 2 for selections
-    fluidRow(
-      column(3,
-             p("Select what data you want", style = "font-weight: bold; color: black;"),  
-             div("All available indicators will be displayed for
-                 selected geography if none specified"),
-             awesomeRadio("product_filter", label=NULL, choices = c("Indicator", "Domain", "Profile"), selected = NULL, inline = FALSE,
-                          status = "primary", checkbox = TRUE),
-             conditionalPanel(condition="input.product_filter=='Indicator'",
-                              selectizeInput("indicator_filter", label = NULL,
-                                             choices = indicator_list, selected = NULL,
-                                             multiple=TRUE, options = list(maxOptions = 1000, placeholder = "Select or type indicators to filter by"))
-                              
-             ),
-             conditionalPanel(condition="input.product_filter=='Domain'",
-                              selectizeInput("topic_filter", label = NULL,
-                                             choices = topic_list, selected = NULL,
-                                             multiple=TRUE, options = list(maxOptions = 1000, placeholder = "Select or type domains to filter by"))
-             ),
-             conditionalPanel(condition="input.product_filter=='Profile'",
-                              selectizeInput("profile_filter", label = NULL,
-                                             choices = profile_list, selected = NULL,
-                                             multiple=TRUE, options = list(maxOptions = 1000, placeholder = "Select or type profiles to filter by"))    
-             )
-             ),# column bracket
-      column(3,
-             p("Select what areas you want", style = "font-weight: bold; color: black;"),
-             # Scotland selections
-             awesomeCheckbox("scotland",label = "Scotland", value = FALSE),
-             # Panel for health board selections
-             awesomeCheckbox("hb",label = "Health board", value = FALSE),
-             conditionalPanel(
-               condition = "input.hb == true",
-               selectInput("hb_true", label = NULL,
-                           choices = hb_name, selected = NULL, multiple=TRUE)),
-             # Panel for council area selections
-             awesomeCheckbox("la", label = "Council area", value = FALSE),
-             conditionalPanel(condition = "input.la == true",
-                              selectizeInput("la_true", label = NULL,
-                                             choices = la_name, selected = NULL, multiple=TRUE, 
-                                             options = list(placeholder = "Select or type council area of interest"))),
-             # Panel for ADP selections
-             awesomeCheckbox("adp",label = "Alcohol & drug partnership", value = FALSE),
-             conditionalPanel(condition = "input.adp == true",
-                              selectizeInput("adp_true", label = NULL,
-                                             choices = adp_name, selected = NULL, multiple=TRUE,
-                                             options = list(placeholder = "Select or type ADP of interest")))  
-             
-      ), # column bracket
-      column(3,
-             br(),
-             # Panel for HSC partnership selections
-             awesomeCheckbox("hscp",label = "Health & social care partnership", value = FALSE),
-             conditionalPanel(
-               condition = "input.hscp == true",
-               selectInput("hscp_true", label = NULL, choices = partnership_name, 
-                           selected = NULL, multiple=TRUE)),
-             # Panel for locality selections
-             awesomeCheckbox("hscl",label = "Health & social care locality", value = FALSE),
-             conditionalPanel(condition = "input.hscl == true",
-                              selectizeInput("hscl_parent", label = "Filter locality list by HSC partnership",
-                                             choices = parent_geo_list, 
-                                             selected = "Show all", multiple=FALSE),
-                              # if they haven't selected all, show tickbox so they can select all localities of parent area
-                              conditionalPanel(condition = "input.hscl_parent != 'Show all'",
-                                               checkboxInput("hscl_parent_all",label = "Select all HSC localities in this area", 
-                                                             value = FALSE)),
-                              uiOutput("hscl_filtered")),
-             # Panel for intermediate zone selections
-             awesomeCheckbox("iz",label = "Intermediate zone", value = FALSE),
-             conditionalPanel(condition = "input.iz == true",
-                              selectizeInput("iz_parent", label = "Filter intermediate zone list by HSC partnership",
-                                             choices = parent_geo_list, selected = "Show all", multiple=FALSE),
-                              # if they haven't selected all, show tickbox so they can select all izs of parent area
-                              conditionalPanel(condition = "input.iz_parent != 'Show all'",
-                                               checkboxInput("iz_parent_all",label = "Select all intermediate zones in this area", value = FALSE)),
-                              uiOutput("iz_filtered")),
-             # To select all available geographies
-             awesomeCheckbox("all_data",label = "All available geographies", value = FALSE),
-             # to search by code
-             selectizeInput("code", label = NULL, choices = code_list,
-                            options = list(placeholder = 'Or search by area code'), 
-                            multiple=TRUE, selected = "")
-      ), #column bracket
-      column(3, style = "width:20%",
-             p("Select the time period", style = "font-weight: bold; color: black;"),
-             sliderInput("date_from",label = NULL, min = min_year, 
-                         max = max_year, value = c(min_year,max_year), 
-                         step = 1, sep="", round = TRUE, 
-                         ticks = TRUE, dragRange = FALSE),
-             br(),
-             actionButton("clear", label = "Clear all filters",  icon ("eraser"), class = "down"),
-             downloadButton("download_table_csv", 'Download data', class = "down")             
-      ) #column bracket
-  ), #filters fluid row bracket
-  #Row 3- Table
-  fluidRow(  
-    column(12, div(DT::dataTableOutput("table_filtered"), 
-                   style = "font-size: 98%; width: 98%"))
-  )
-    ) # main panel bracket
-  ) #Tab panel bracket  
+  layout_sidebar(padding = 20, gap = 20,
+    sidebar = sidebar(width = 300, padding = 20,
+
+        h2("Filters"),
+
+        # clear filters button
+        actionButton("clear_table_filters",
+                     label = "Clear all filters",
+                     icon ("eraser"),
+                     class = "down"),
+
+        # dataset selection filter
+        radioGroupButtons(
+          inputId = "dataset_selector",
+          label = "Choose a dataset",
+          choices = c("Main Dataset",
+                      "Inequalities Dataset"),
+          selected = "Main Dataset" # default on main opt dataset
+        ),
+
+        # quintile type filter (only if inequalities dataset is selected)
+        conditionalPanel(
+          condition = "input['dataset_selector'] === 'Inequalities Dataset'",
+          radioGroupButtons(
+            inputId = "quint_type_selector",
+            label = "Select quintile type",
+            choices = c("Local", "Scotland"),
+            selected = "Scotland" # default on max year for each indicator
+          )
+        ),
+
+        # Geography filters
+        jstreeOutput("geography_selector"),
+
+        # profile filters
+        virtualSelectInput(inputId = "profile_selector",
+                           label = "Select profile(s)",
+                           choices = profile_list,
+                           disableSelectAll = FALSE,
+                           multiple = TRUE,
+                           search = TRUE,
+                           searchByStartsWith = TRUE,
+                           width = '100%',
+                           zIndex = 10),
+
+        # indicator filters
+        virtualSelectInput(inputId = "indicator_selector",
+                           label = "Select indicator(s)",
+                           noOptionsText = "Select atleast one geography to see what indicators are available",
+                           choices = NULL,
+                           disableSelectAll = FALSE,
+                           multiple = TRUE,
+                           search = TRUE,
+                           searchByStartsWith = TRUE,
+                           width = '100%',
+                           zIndex = 10),
+ 
+
+        # time period filter
+        radioGroupButtons(
+          inputId = "time_period_selector",
+          label = "Select time period:",
+          choices = c("Latest available year", "Trends"),
+          selected = "Latest available year"
+        )
+
+      ), # close sidebar
+
+    h1("Data table"),
+    p("Use the filters to build a data table, which can then be downloaded in various
+    formats using the button below. Please note that the table below is a preview. The downloaded dataset will contain more columns containing metadata than are presented here."),
+    
+    # download data button
+    downloadDataButtonsUI(id = "datatable_downloads"),
+    # data table
+    DTOutput("data_tab_table")
+
+  ) # close layout
+) # close tab panel
+
+
+### END
+  
